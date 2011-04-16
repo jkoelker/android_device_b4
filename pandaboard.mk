9ef4fafaf1838a82d21fbe58a8b99d02d7ee0b72
Jeremy Chang <jeremy.chang@linaro.org>
2011-04-16 19:15:23 +0800

    Move the common package and benchmark definitions into common folder
    Signed-off-by: Jeremy Chang <jeremy.chang@linaro.org>

--- pandaboard.mk
+++ pandaboard.mk
@@ -1,36 +1,7 @@
 # The pandaboard product that is specialized for pandaboard.
-
-include $(LOCAL_PATH)/ZeroXBenchmark.mk
-
-PRODUCT_PACKAGES := \
-    ZeroXBenchmark \
-    libmicro \
-    AccountAndSyncSettings \
-    AlarmClock \
-    AlarmProvider \
-    Bluetooth \
-    Calculator \
-    Calendar \
-    Camera \
-    CertInstaller \
-    DrmProvider \
-    Email \
-    Gallery3D \
-    LatinIME \
-    Mms \
-    Music \
-    Provision \
-    Settings \
-    Sync \
-    Updater \
-    CalendarProvider \
-    SyncProvider \
-    $(ZEROXBENCHMARK_NATIVE_APPS)
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
+$(call inherit-product, device/linaro/common/common.mk)
 $(call inherit-product, device/linaro/pandaboard/device.mk)
 
-# Overrides
 PRODUCT_BRAND := pandaboard
 PRODUCT_DEVICE := pandaboard
 PRODUCT_NAME := pandaboard
