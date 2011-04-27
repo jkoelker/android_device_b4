6b0cab83ef1b25d0b05dc8e54cf0dc3e3daf1706
Jim Huang <jim.huang@linaro.org>
2011-04-27 21:56:30 +0800

    Re-organize the installation rules for PowerVR SGX binaries
    The previous rules were neat, but we encountered strange failure when
    using parallel build.  Switch back to simplified rules instead.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -20,12 +20,6 @@ LOCAL_SRC_FILES := Logitech_Logitech_USB_Keyboard.kcm
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_KEY_CHAR_MAP)
 
-
-file := $(TARGET_ROOT_OUT)/system/lib/egl/egl.cfg
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/egl.cfg | $(ACP)
-        $(transform-prebuilt-to-target)
-
 file := $(TARGET_ROOT_OUT)/init.rc
 ALL_PREBUILT += $(file)
 $(file) : $(LOCAL_PATH)/../common/init.rc | $(ACP)
@@ -45,3 +39,6 @@ file := $(TARGET_ROOT_OUT)/initlogo.rle
 ALL_PREBUILT += $(file)
 $(file) : $(LOCAL_PATH)/initlogo.rle | $(ACP)
 	$(transform-prebuilt-to-target)
+
+# PowerVR SGX binaries
+include device/linaro/pandaboard/prebuilt/graphics/sgx.mk
