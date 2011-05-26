10c4dcab130cb9160cefffbeea941d7093223f16
Jim Huang <jim.huang@linaro.org>
2011-05-27 06:02:08 +0800

    Don't depend on beagleabord's vold.fstab
    Also, fix the incorrect vold.fstab definition.

--- device.mk
+++ device.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 PRODUCT_COPY_FILES := \
-    device/linaro/beagleboard/vold.fstab:system/etc/vold.fstab \
+    device/linaro/pandaboard/vold.fstab:system/etc/vold.fstab \
     device/linaro/pandaboard/egl.cfg:system/lib/egl/egl.cfg
 
 PRODUCT_COPY_FILES += \
