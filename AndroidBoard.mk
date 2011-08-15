1a15c6686959d419633c3a02c22084fe99659da4
Jason Kölker <jason@koelker.net>
2011-08-14 22:00:00 -0500

    more cleanup

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,28 +1,29 @@
 LOCAL_PATH := $(call my-dir)
+PANDA_PATH := $(call my-dir)../../linaro/pandaboard
 
 file := $(TARGET_OUT_KEYLAYOUT)/gpio-keys.kl
 ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/gpio-keys.kl | $(ACP)
+$(file) : $(PANDA_PATH)/gpio-keys.kl | $(ACP)
 	$(transform-prebuilt-to-target)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := gpio-keys.kcm
+LOCAL_SRC_FILES := $(PANDA_PATH)/gpio-keys.kcm
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_KEY_CHAR_MAP)
 
 file := $(TARGET_OUT_KEYLAYOUT)/Logitech_Logitech_USB_Keyboard.kl
 ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/Logitech_Logitech_USB_Keyboard.kl | $(ACP)
+$(file) : $(PANDA_PATH)/Logitech_Logitech_USB_Keyboard.kl | $(ACP)
 	$(transform-prebuilt-to-target)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := Logitech_Logitech_USB_Keyboard.kcm
+LOCAL_SRC_FILES := $(PANDA_PATH)/Logitech_Logitech_USB_Keyboard.kcm
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_KEY_CHAR_MAP)
 
 file := $(TARGET_ROOT_OUT)/init.rc
 ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/../common/init.rc | $(ACP)
+$(file) : $(PANDA_PATH)/../common/init.rc | $(ACP)
 	$(transform-prebuilt-to-target)
 
 file := $(TARGET_ROOT_OUT)/init.omap4430.rc
